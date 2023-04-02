0efb773eb7b6134ff685bd2d90abf403332b54c4
JulesBest176 <souleymanefall176@gmail.com>
2023-04-02 19:01:08 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*typing animation */
+var typed = new Typed(".typing", {
+    strings: ["", "web Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+})
+/*typing animation */
+var typed2 = new Typed(".name", {
+    strings: ["", "Souleymane Fall"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+})
