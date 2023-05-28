f036879058cf247da68a77a2403411a65c9dce65
JulesBest176 <souleymanefall176@gmail.com>
2023-05-28 21:28:54 +0000

    portfolio remake

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 /*typing animation */
 var typed = new Typed(".typing", {
-    strings: ["", "web Developer"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true,
-})
+  strings: ["", "web Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
 /*typing animation */
 var typed2 = new Typed(".name", {
-    strings: ["", "Souleymane Fall"],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop: true,
-})
+  strings: ["", "Souleymane Fall"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+var portfolioImages = document.querySelectorAll(".portfolio-img");
+
+portfolioImages.forEach(function (portfolioImg) {
+  var image = portfolioImg.querySelector("img");
+
+  image.addEventListener("click", function () {
+    if (image.requestFullscreen) {
+      image.requestFullscreen();
+    } else if (image.mozRequestFullScreen) {
+      image.mozRequestFullScreen();
+    } else if (image.webkitRequestFullscreen) {
+      image.webkitRequestFullscreen();
+    } else if (image.msRequestFullscreen) {
+      image.msRequestFullscreen();
+    }
+  });
+});
